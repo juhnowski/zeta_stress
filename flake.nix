a18a544a2b3b3b10273519f07ca0f4e60641b527
juhnowski <juhnowski@gmail.com>
2026-07-01 14:52:06 +0300

    influxdb version

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Riemann Zeta function heavy stress-test in Haskell on NixOS (Odlyzko-Schönhage FFT Optimization)";
+  description = "Riemann Zeta function heavy stress-test in Haskell with InfluxDB on NixOS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -19,17 +19,28 @@
           };
         };
 
+        # Haskell-зависимости проекта
         hpkgs = with haskellPackages; [
           parallel
           async
           vector
           vector-fftw
+          wreq         # Библиотека для простых и быстрых HTTP-запросов
+          http-client  # Базовый HTTP-движок
+          bytestring   # Эффективная работа с бинарными строками для отправки в БД
+          text         # Текстовые типы данных
         ];
         
-        # Решение проблемы: Используем актуальный и поддерживаемый LLVM 18
+        # Актуальный и поддерживаемый LLVM 18 для сборщика GHC
         llvmSuite = pkgs.llvmPackages_18;
+
+        # Python-окружение для работы с InfluxDB
+        pythonEnv = pkgs.python3.withPackages (ps: [
+          ps.influxdb-client 
+        ]);
       in
       {
+        # Интегрированное окружение разработки (devShell)
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             (haskellPackages.ghcWithPackages (p: hpkgs))
@@ -38,15 +49,41 @@
             llvmSuite.llvm
             llvmSuite.clang
             htop
+            
+            # Перенесенные зависимости InfluxDB и Python
+            influxdb2
+            influxdb2-cli
+            pythonEnv
           ];
+
+          # Объединенный shellHook для работы с InfluxDB прямо в dev-окружении
+          shellHook = ''
+            export INFLUX_DIR="$PWD/influx_data"
+            export PORT=8086
+            
+            # Настройки агрессивного сброса кэша на диск в TSM-файлы из вашего проекта
+            export INFLUX_STORAGE_CACHE_SNAPSHOT_MEMORY_SIZE=16777216  # 16 MB
+            export INFLUX_STORAGE_CACHE_SNAPSHOT_WRITE_COLD_DURATION=1s # 1 сек
+
+            mkdir -p "$INFLUX_DIR/engine"
+
+            echo "--------------------------------------------------------"
+            echo " Доступные команды InfluxDB-стенда:"
+            echo "   start-influx - Запустить локальный сервер InfluxDB"
+            echo "   stop-influx  - Безопасно остановить сервер InfluxDB"
+            echo "--------------------------------------------------------"
+
+            alias start-influx="influxd --bolt-path=\$INFLUX_DIR/influxd.bolt --engine-path=\$INFLUX_DIR/engine --http-bind-address=127.0.0.1:\$PORT > \$INFLUX_DIR/stdout.log 2>&1 & echo \$! > \$INFLUX_DIR/influxd.pid && echo '[+] InfluxDB запущен в фоне.'"
+            alias stop-influx="if [ -f \$INFLUX_DIR/influxd.pid ]; then kill \$(cat \$INFLUX_DIR/influxd.pid) && rm \$INFLUX_DIR/influxd.pid && echo '[+] InfluxDB остановлен.'; else echo '[!] PID-файл не найден.'; fi"
+          '';
         };
 
+        # Сборка финального оптимизированного бинарника математического движка
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "zeta_stress";
-          version = "1.0.0";
+          version = "1.0.1";
           src = ./.;
 
-          # Прокидываем корректные бинарники компилятора и утилит LLVM 18
           nativeBuildInputs = [ 
             llvmSuite.llvm
             llvmSuite.clang
--- zeta_stress.hs
+++ zeta_stress.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 import Control.Concurrent.Async (mapConcurrently)
--- Импортируем Control.Parallel.Strategies для гарантированного параллелизма
 import Control.Parallel.Strategies (withStrategy, parListChunk, rdeepseq)
 import qualified Data.Vector.Storable as V
 import qualified Numeric.FFT.Vector.Invertible as FFT
@@ -11,22 +11,40 @@ import Data.List (foldl')
 import GHC.Conc (numCapabilities)
 import System.Environment (getArgs)
 import Text.Printf (printf)
+import Control.Monad (zipWithM_) -- Обязательно для последовательной индексированной отправки
+
+-- Импорты для работы с InfluxDB HTTP API
+import qualified Network.Wreq as W
+import Control.Lens ((&), (.~))
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BL
+import Network.HTTP.Client (defaultManagerSettings, newManager)
 
 type ComplexDouble = Complex Double
 
-prepareCoefficients :: Double -> Int -> Int -> V.Vector ComplexDouble
-prepareCoefficients !t0 !nMin !nMax = V.generate (nMax - nMin + 1) $ \idx ->
-    let n   = fromIntegral (nMin + idx)
-        phi = -t0 * log n
-        baseTerm = (1.0 / sqrt n) :+ 0.0
-        phaseShift = exp (0.0 :+ phi)
-    in baseTerm * phaseShift
+-- Собственная реализация разбиения списка на чанки без внешних зависимостей
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = []
+chunksOf n xs = let (first, rest) = splitAt n xs in first : chunksOf n rest
 
+-- | 1. Математическое ядро: подготовка коэффициентов с Zero Padding до размера FFT-окна
+prepareCoefficients :: Int -> Double -> Int -> Int -> V.Vector ComplexDouble
+prepareCoefficients !totalSamples !t0 !nMin !nMax = V.generate totalSamples $ \idx ->
+    let currentN = nMin + idx
+    in if currentN <= nMax
+       then let n = fromIntegral currentN
+                phi = -t0 * log n
+                baseTerm = (1.0 / sqrt n) :+ 0.0
+                phaseShift = exp (0.0 :+ phi)
+            in baseTerm * phaseShift
+       else 0.0 :+ 0.0
+
+-- | 2. Оптимизированный блочный расчет через БПФ с выровненными векторами
 evalBlockOD :: Double -> Double -> Int -> V.Vector Double
 evalBlockOD !t0 !deltaT !samples = 
     let nMax = floor (sqrt ((t0 + deltaT) / (2 * pi)))
         nMin = 1
-        coeffs = prepareCoefficients t0 nMin nMax
+        coeffs = prepareCoefficients samples t0 nMin nMax
         fftResult = FFT.run FFT.dft coeffs
         theta !t = (t / 2) * log (t / (2 * pi)) - (t / 2) - (pi / 8)
     in V.imap (\idx complexVal ->
@@ -35,6 +53,7 @@ evalBlockOD !t0 !deltaT !samples =
         in 2 * realPartVal
        ) fftResult
 
+-- | 3. Поиск нулей внутри вычисленного вектора значений Z-функции
 findZerosInVector :: Double -> Double -> V.Vector Double -> [Double]
 findZerosInVector !t0 !deltaT !vals = go 0 []
   where
@@ -51,19 +70,57 @@ findZerosInVector !t0 !deltaT !vals = go 0 []
              then go (i + 1) (tZero : acc)
              else go (i + 1) acc
 
--- | ОПТИМИЗАЦИЯ: Строгий параллельный конвейер без ленивых задержек
+-- | 4. Строгий параллельный конвейер без ленивых задержек планировщика
 processPipeline :: Double -> Double -> Double -> IO [Double]
 processPipeline !globalStart !globalEnd !blockSize = do
     let !blocks = [ (t, min globalEnd (t + blockSize)) 
                   | t <- [globalStart, globalStart + blockSize .. globalEnd - 1] ]
-        samplesPerBlock = 1048576 
+        samplesPerBlock = 524288 
     
-    -- Вычисляем блоки строго параллельными чанками, заставляя GHC нагружать все ядра сразу
-    let !chunksOfZeros = withStrategy (parListChunk 8 rdeepseq) $ map (\(s, e) ->
+    let !chunksOfZeros = withStrategy (parListChunk 16 rdeepseq) $ map (\(s, e) ->
                            findZerosInVector s (e - s) (evalBlockOD s (e - s) samplesPerBlock)
                          ) blocks
                          
     return $ concat chunksOfZeros
+
+-- | 5. СТАБИЛЬНАЯ ФУНКЦИЯ ЗАПИСИ (Последовательный стриминг батчей через zipWithM_)
+sendToInflux :: String -> [Double] -> IO ()
+sendToInflux _ [] = return ()
+sendToInflux blockNumStr zeros = do
+    let host = "http://127.0.0.1:8086"
+        path = "/api/v2/write"
+        queryOrg = "?org=zeta_org"
+        queryBucket = "&bucket=zeta_bucket"
+        queryPrecision = "&precision=ns"
+        url = host ++ path ++ queryOrg ++ queryBucket ++ queryPrecision
+
+    let token = "ZetaStressSecretToken2026FFFFFFFFFFFF" 
+        
+        opts = W.defaults 
+             & W.header "Authorization" .~ [BS.pack ("Token " ++ token)]
+             & W.header "Content-Type" .~ [BS.pack "text/plain; charset=utf-8"]
+
+    let sizeBatches = 200000
+        chunks = chunksOf sizeBatches zeros
+        totalChunks = length chunks
+
+    putStrLn $ "Отправка в InfluxDB (" ++ show totalChunks ++ " батчей по " ++ show sizeBatches ++ " точек)..."
+
+    let indices = [1 .. totalChunks] :: [Int]
+    
+    -- Последовательный обход: защищает локальный сервер InfluxDB от сетевого перегруза
+    zipWithM_ (\idx chunk -> do
+        let payloadLines = map (\z -> "riemann_zeta,block=" ++ blockNumStr ++ " value=" ++ show z) chunk
+            payloadBody  = BL.pack (unlines payloadLines)
+        
+        _ <- W.postWith opts url payloadBody
+        
+        if idx `mod` 50 == 0 || idx == totalChunks
+            then printf "  [Запись] Доставлено батчей: %d/%d\n" idx totalChunks
+            else return ()
+      ) indices chunks
+
+    putStrLn "[+] Все батчи успешно доставлены в InfluxDB без перегрузки сервера."
 
 main :: IO ()
 main = do
@@ -72,7 +129,7 @@ main = do
         [blockNumStr, startStr, endStr] -> do
             let tStart    = read startStr :: Double
                 tEnd      = read endStr :: Double
-                blockSize = 10000.0 -- Уменьшили размер подблока для лучшей балансировки ядер
+                blockSize = 10000.0 
             
             allZeros <- processPipeline tStart tEnd blockSize
             
@@ -82,6 +139,8 @@ main = do
             let filePath = "riemann_chunks_summary.csv"
             withFile filePath AppendMode $ \handle -> do
                 hPutStrLn handle $ blockNumStr ++ ";" ++ startStr ++ ";" ++ endStr ++ ";" ++ show totalCount
+                
+            sendToInflux blockNumStr allZeros
                 
             putStrLn "========================================"
             printf "Блок №%s успешно обработан.\n" blockNumStr
